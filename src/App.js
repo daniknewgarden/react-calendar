9da2a6baac9d2a637ab1b91b706b3fa85252574e
Danik <daniknewgarden@gmail.com>
2020-12-23 22:43:17 +0200

    Divided into components

--- src/App.js
+++ src/App.js
@@ -1,73 +1,11 @@
 import "./App.scss";
-import React, { useState, useEffect } from "react";
-
 //components
 import { RangeDatePicker } from "./components/DatePicker/DatePicker";
-import "react-dates/initialize";
-import { DateRangePicker } from "react-dates";
-import "react-dates/lib/css/_datepicker.css";
-import { Button } from "./components/Button/Button";
-
-import { VERTICAL_ORIENTATION } from "react-dates/constants";
-import moment from "moment";
 
 function App() {
-  const [focusedInput, setFocusedInput] = useState(null);
-  const [date, setDate] = useState({
-    startDate: null,
-    endDate: null,
-  });
-
-  const changeDates = ({ startDate, endDate }) => {
-    setDate({ startDate, endDate });
-  };
-
-  const resetDate = () => {
-    setDate({ startDate: null, endDate: null });
-  };
-
-  const focusStartDate = () => {
-    setFocusedInput("startDate");
-  };
-
-  const keyboardClick = (event) => {
-    if (event.keyCode === 13) {
-      focusStartDate();
-    }
-  };
-
   return (
     <div className="App">
-      <div
-        className="controls"
-        onClick={focusStartDate}
-        onKeyDown={keyboardClick}
-        tabIndex="0"
-      >
-        <div className="text">
-          <p className="title">Choose your date</p>
-          <p className="subtitle">
-            {date.startDate ? date.startDate.format("D-MM-Y") : "Start date"} -{" "}
-            {date.endDate ? date.endDate.format("D-MM-Y") : "End date"}
-          </p>
-        </div>
-        {date.startDate && <Button label="Reset" onClick={resetDate} />}
-      </div>
-      <DateRangePicker
-        startDate={date.startDate}
-        startDateId="start_date_id"
-        endDate={date.endDate}
-        endDateId="end_date_id"
-        onDatesChange={changeDates}
-        focusedInput={focusedInput}
-        onFocusChange={(focusedInput) => {
-          setFocusedInput(focusedInput);
-        }}
-        block={true}
-        small={true}
-        orientation={VERTICAL_ORIENTATION}
-        keepOpenOnDateSelect={true}
-      />
+      <RangeDatePicker />
     </div>
   );
 }
--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -1,26 +1,92 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+//React-dates
 import "react-dates/initialize";
 import { DateRangePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
+import { VERTICAL_SCROLLABLE } from "react-dates/constants";
+//Components
+import { Button } from "../Button/Button";
+//Styles
+import "./DatePicker.scss";
 
 export const RangeDatePicker = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  const [opened, setOpened] = useState(false);
   const [focusedInput, setFocusedInput] = useState(null);
+  const [date, setDate] = useState({
+    startDate: null,
+    endDate: null,
+  });
 
-  const dateChange = (start, end) => {
-    console.log(startDate, endDate);
+  //Toggle date picker
+  const toggleOpened = () => {
+    setOpened(!opened);
+  };
+
+  //When user select date (start or end)
+  const changeDates = ({ startDate, endDate }) => {
+    setDate({ startDate, endDate });
+  };
+
+  //Cleaning (call from reset button)
+  const resetDate = () => {
+    setDate({ startDate: null, endDate: null });
+  };
+
+  //Open or close Range component (payload = "startDate": opened, null: closed)
+  const focusStartDate = (payload) => {
+    setFocusedInput(payload);
+  };
+
+  useEffect(() => {
+    opened ? focusStartDate("startDate") : focusStartDate(null);
+  }, [opened]);
+
+  //Click from focus on controls div
+  const keyboardClick = (event) => {
+    switch (event.keyCode) {
+      case 13:
+        toggleOpened();
+        break;
+      case 32:
+        toggleOpened();
+        break;
+      default:
+        break;
+    }
   };
 
   return (
-    <DateRangePicker
-      startDate={startDate}
-      startDateId="start_date_id"
-      endDate={endDate}
-      endDateId="end_date_id"
-      onDatesChange={({ startDate, endDate }) => dateChange(startDate, endDate)}
-      focusedInput={focusedInput}
-      onFocusChange={(focusedInput) => console.log(focusedInput)}
-    />
+    <div className="date-picker">
+      <div
+        className="controls"
+        onClick={toggleOpened}
+        onKeyDown={keyboardClick}
+        tabIndex="0"
+      >
+        <div className="text">
+          <p className="title">Choose your date</p>
+          <p className="subtitle">
+            {date.startDate ? date.startDate.format("D-MM-Y") : "Start date"} -{" "}
+            {date.endDate ? date.endDate.format("D-MM-Y") : "End date"}
+          </p>
+        </div>
+        {date.startDate && <Button label="Reset" onClick={resetDate} />}
+      </div>
+      <DateRangePicker
+        startDate={date.startDate}
+        startDateId="start_date_id"
+        endDate={date.endDate}
+        endDateId="end_date_id"
+        onDatesChange={changeDates}
+        focusedInput={focusedInput}
+        onFocusChange={(focusedInput) => {
+          setFocusedInput(focusedInput);
+        }}
+        block={true}
+        small={true}
+        orientation={VERTICAL_SCROLLABLE}
+        numberOfMonths={6}
+      />
+    </div>
   );
 };
